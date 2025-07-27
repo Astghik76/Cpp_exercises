1d6ce00f4e1c80205ef0406d7a1bff3c4306c727
Astx Janinyan <janinyanastx@gmail.com>
2025-07-27 12:59:01 +0400

    Add Hw42 c++exercise

---
+++ Hw42/crazy_sequence.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int F(int i) {
+ if (i == 0) return 0;
+  if (i == 1) return 5;
+   if (i == 2) return 3;
+     if (i == 3) return 4;
+        if (i == 4) return 4;
+ return F(i - 5) * F(i - 4) + F(i - 2);
+}
+
+int main() {
+int N;
+ std::cout << "Enter N: ";
+ std::cin >> N;
+
+ std::cout << "F[" << N << "] = " << F(N) << "\n";
+
+  return 0;
+}
+
